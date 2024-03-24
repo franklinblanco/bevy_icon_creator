e4cf2899173f9b28e722a2f4c1b4e5e98a0e0ce5
Franklin <franklinblanco@tutanota.com>
2024-03-24 15:08:37 +0100

    Exported setimageonloadmarker

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ mod update;
 mod setup;
 mod state;
 mod utils;
-mod set_image_on_load;
 mod register_types;
 mod hash_to_uuid_ext;
 
@@ -11,3 +10,4 @@ pub mod image_ext;
 pub mod plugin;
 pub mod needs_icon_marker;
 pub mod created_icons;
+pub mod set_image_on_load;
